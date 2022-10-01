9f1f313d7947da352803001c2c86955ff083ff99
skkim-01 <lemoned222@gmail.com>
2022-10-01 16:56:30 +0900

    Server: test for ssh client
    // TODO: siba

---
+++ server/objects/sshclient/sshclient.js
@@ -0,0 +1,128 @@
+const { Client }    = require('ssh2');
+const { EventEmitter } = require('events')
+const { conv } = require('../../../common/util/conv')
+
+class SSHClient {
+    // TODO: is it need?
+    eventEmitter = new EventEmitter()
+    sshClient = new Client()
+
+    outStream
+
+    _handleOutputMessage(message) {
+        return message
+    }
+    oneshotexe(cmd) {
+        console.log('oneshotexe')
+        this.eventEmitter.emit('execute', 'ls -al')
+    }
+
+    oneshot(jsonOptions) {
+        var conn = new Client()
+        conn.on('ready', () => {
+            console.log('Client :: ready');
+            conn.shell((err, stream) => {
+                if (err) throw err;
+    
+                stream.on('close', () => {
+                        console.log('Stream :: close');                        
+                    }).on('data', (data) => {
+                        //console.log( conv.buf2utf8(data) );
+                        console.log( data );
+                        
+                    });
+
+                    this.eventEmitter.on('execute', (msg) => {
+                        console.log('emitted')
+                        console.log(msg)
+                        stream.end()
+                    })
+                    
+                    //stream.push('ls -al', "utf-8")
+                    //stream.end('ls -l');
+                });
+            }).connect(jsonOptions);
+    }
+
+    async shellMsg() {
+        return new Promise( (resolve, reject) => {
+            var out = ''
+            this.sshClient.shell((err, stream) => {
+                if ( err ) resolve(err)
+                stream.on('close', () => {
+                    console.log('stream close')  
+                }).on('data', (data) => {                    
+                    console.log( conv.buf2utf8(data).trim() + '\n' )
+                    //console.log( data.length )
+                    //console.log(stream.readableLength)
+                }).on('end', () => {
+                    resolve('end')
+                })
+                stream.end();
+            });
+        })
+    }
+
+    async connect(jsonOptions) {
+        this.sshClient.connect(jsonOptions)
+
+        return new Promise( (resolve, reject) => {            
+            this.sshClient.on('ready', () => {
+                //resolve(this.shellMsg())
+                resolve('test')
+            })
+
+            this.sshClient.on('banner', (msg) => {
+                console.log(msg)
+            })
+
+            this.sshClient.on('greeting', (msg) => {
+                console.log(msg)
+            })
+    
+            this.sshClient.on('error', (msg) => {
+                console.log("\tEV/ERROR", msg)
+                resolve('ERROR')
+            })
+
+            this.sshClient.on('close', () => {                
+                this.eventEmitter.emit('message', 'EV/Close')
+            })
+    
+            this.sshClient.on('end', (msg) => {                
+                this.eventEmitter.emit('message', 'EV/end')
+            })
+        })
+    }
+
+    async disconnect() {
+        this.sshClient.end()
+
+        return new Promise( (resolve, reject) => {
+            this.eventEmitter.once('message', (message) => {
+                resolve( this._handleOutputMessage(message) )
+            })
+        })
+    }
+
+    async exec(cmd) {
+        var msg = ''
+        return new Promise( (resolve, reject) => {
+            this.sshClient.exec( cmd, function(err, stream) {
+                if (err) throw err;
+
+                stream.on('close', function(code, signal) {                    
+                    resolve(conv.buf2utf8(msg))
+                }).on('data', function(data) {
+                    console.log('stdout:')
+                    msg = data
+                }).stderr.on('data', function(data) {
+                    console.log('stderr')
+                    msg = data
+                });
+            })
+        })
+    }
+}
+
+module.exports = { SSHClient }
---
+++ server/objects/sshclient/test.js
@@ -0,0 +1,58 @@
+const { SSHClient } = require('./sshclient')
+
+// var ssh = new SSHClient()
+// ssh.oneshot({
+//   host : "192.168.219.107",
+//   port : 22,
+//   user : "test",
+//   password : "1"
+// })
+
+// // end of process
+// const exec = () => new Promise((resolve, reject) => {
+//   setTimeout(() => {resolve(
+//     ssh.oneshotexe()
+//   )}, 3000);
+//   // TODO: Close code
+// });
+
+// exec()
+
+main()
+
+async function main() {
+  var default_opt =  {
+    host : "192.168.219.107",
+    port : 22,
+    user : "test",
+    password : "1"
+  }
+
+  console.log("1: new sshclient")
+  ssh = new SSHClient()
+
+  console.log("2: connect")
+  var retv = await ssh.connect(default_opt)
+  console.log("2: connect.result", retv)
+
+  console.log("3: exec")
+  var retv = await ssh.exec('ls')
+  console.log("3: exec.result")
+  console.log(retv)
+
+  // console.log("3: exec")
+  // var retv = await ssh.exec('ls -al')
+  // console.log("3: exec.result")
+  // console.log(retv)
+
+  // console.log("3: exec")
+  // var retv = await ssh.exec('lfaqwes -al')
+  // console.log("3: exec.result")
+  // console.log(retv)
+
+  console.log("4: disconnect")
+  var retv = await ssh.disconnect()
+  console.log("4: disconnect.result", retv)
+
+  //console.log("5: fin")
+}
